c3f165cecd29f087eaaa16e6048532da242ccfdc
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-08-04 19:26:12 +0500

    Characters can now be displayed on calculator screen

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ function operate(operator, number1, number2) {
     return operators[operator](number1, number2);
 }
 
+const displayScreen = document.querySelector(".display-screen");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const buttonClicked = event.target;
+
+        if (buttonClicked.innerText === "AC") {
+            displayScreen.innerText = "";
+        } 
+        else if (buttonClicked.innerText === "Del" && buttonClicked.innerText.length > 0) {
+            displayScreen.innerText = displayScreen.innerText.slice(0, displayScreen.innerText.length - 1);
+        } 
+        else {
+            if (displayScreen.innerText.length === 10) {
+                alert("Cannot enter more than 10 characters");
+                return;
+            }
+            displayScreen.innerText += buttonClicked.innerText;
+        }
+    });
+});
+
