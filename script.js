f8828849384f850df9058f32448c0ae3f56e17f0
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-08-05 18:23:44 +0500

    Start work on solving expressions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const displayScreen = document.querySelector(".display-screen");
+const buttons = document.querySelectorAll(".button");
+let expressionString = "";
+
 const operatorFunctions = {
     "+": (number1, number2) => number1 + number2,
     "-": (number1, number2) => number1 - number2,
@@ -9,27 +13,46 @@ function operate(operator, number1, number2) {
     return operatorFunctions[operator](number1, number2);
 }
 
+function solveExpression() {
+    let expressionArray = expressionString.split(" ");
+    let numbersArray = expressionArray.filter((element) => !(element in operatorFunctions));
+    let operatorsArray = expressionArray.filter((element) => element in operatorFunctions);
+}
 
-const displayScreen = document.querySelector(".display-screen");
-const buttons = document.querySelectorAll(".button");
+
+function setButtonFunctions(event) {
+    const buttonClicked = event.target;
+
+    if (buttonClicked.textContent === "AC") {
+        expressionString = "";
+    } 
+    else if (buttonClicked.textContent === "Del") {
+        if (expressionString.length <= 0) return;
+
+        expressionString = expressionString.slice(0, expressionString.length - 1);
+
+        if (expressionString[expressionString.length - 1 ] in operatorFunctions) {
+            expressionString = expressionString.slice(0, expressionString.length - 2);
+        }
+    } 
+    else if (buttonClicked.textContent === "=") {
+        solveExpression();
+    }
+    else {
+        if (expressionString.split(" ").join("").length >= 10) return;
+        if (buttonClicked.textContent in operatorFunctions) {
+            expressionString += ` ${buttonClicked.textContent} `;
+        } else {
+            expressionString += buttonClicked.textContent;
+        }
+    }
+
+    // Remove spaces from the expression
+    displayScreen.textContent = expressionString.split(" ").join("");
+}
+
 
 buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        const buttonClicked = event.target;
-
-        if (buttonClicked.innerText === "AC") {
-            displayScreen.innerText = "";
-        } 
-        else if (buttonClicked.innerText === "Del" && buttonClicked.innerText.length > 0) {
-            displayScreen.innerText = displayScreen.innerText.slice(0, displayScreen.innerText.length - 1);
-        } 
-        else {
-            if (displayScreen.innerText.length === 10) {
-                alert("Cannot enter more than 10 characters");
-                return;
-            }
-            displayScreen.innerText += buttonClicked.innerText;
-        }
-    });
+    button.addEventListener("click", setButtonFunctions);
 });
 
